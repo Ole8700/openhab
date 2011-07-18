ea0c46b7189e886537360852d601ecdf74dc1a94
Kai <Kai@localhost>
2011-07-18 14:14:35 +0200

    removed misterhouse configuration file support

---
+++ bundles/model/org.openhab.model.sitemap/src-gen/org/openhab/model/sitemap/SitemapModel.java
@@ -0,0 +1,23 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.openhab.model.sitemap;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Model</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.openhab.model.sitemap.SitemapPackage#getSitemapModel()
+ * @model
+ * @generated
+ */
+public interface SitemapModel extends EObject
+{
+} // SitemapModel
