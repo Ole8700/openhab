548dfab4e5ac8a6e9de97cf64cdebe6c5426a468
Kai <Kai@localhost>
2011-09-12 22:05:57 +0200

    fixed problem with removal of events from knowledge session

---
+++ bundles/core/org.openhab.core/src/main/java/org/openhab/core/events/AbstractEventSubscriber.java
@@ -0,0 +1,81 @@
+/**
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2011, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+
+package org.openhab.core.events;
+
+import static org.openhab.core.events.EventConstants.TOPIC_PREFIX;
+import static org.openhab.core.events.EventConstants.TOPIC_SEPERATOR;
+
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.openhab.core.types.EventType;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+
+abstract public class AbstractEventSubscriber implements EventSubscriber, EventHandler {
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void handleEvent(Event event) {  
+		String itemName = (String) event.getProperty("item");
+		
+		String topic = event.getTopic();
+		String[] topicParts = topic.split(TOPIC_SEPERATOR);
+		
+		if(!(topicParts.length > 2) || !topicParts[0].equals(TOPIC_PREFIX)) {
+			return; // we have received an event with an invalid topic
+		}
+		String operation = topicParts[1];
+		
+		if(operation.equals(EventType.UPDATE.toString())) {
+			State newState = (State) event.getProperty("state");
+			if(newState!=null) receiveUpdate(itemName, newState);
+		}
+		if(operation.equals(EventType.COMMAND.toString())) {
+			Command command = (Command) event.getProperty("command");
+			if(command!=null) receiveCommand(itemName, command);
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void receiveCommand(String itemName, Command command) {
+		// default implementation: do nothing
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void receiveUpdate(String itemName, State newState) {
+		// default implementation: do nothing
+	}
+
+}
