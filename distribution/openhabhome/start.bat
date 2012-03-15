070c534ef5ced829d1887c6dbedc4493792efb87
teichsta <teichsta@localhost>
2012-03-15 11:38:53 +0100

    changed default item to reflect uninitialized state more precisely

---
+++ distribution/openhabhome/start.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: set path to eclipse folder. If local folder, use '.'; otherwise, use c:\path\to\eclipse
+set ECLIPSEHOME=server
+ 
+:: get path to equinox jar inside ECLIPSEHOME folder
+for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSEHOME%\plugins\org.eclipse.equinox.launcher_*.jar') do set EQUINOXJAR=%%c
+ 
+:: start Eclipse w/ java
+echo Launching the openHAB runtime...
+java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Djetty.port=8080 -Djetty.port.ssl=8443 -Djetty.home=. -Dlogback.configurationFile=logs/logback.xml -Dfelix.fileinstall.dir=addons -Djava.library.path=lib -Djava.security.auth.login.config=./etc/login.conf -Dorg.quartz.properties=./etc/quartz.properties -jar %EQUINOXJAR% %* -console
